b532bb0f072342c9a7127d54daddbdfcd39d7ea0
aiahsan <70939313+aiahsan@users.noreply.github.com>
2021-09-22 08:34:48 -0700

    UPDATE

--- utiles/baseUrl.jsx
+++ utiles/baseUrl.jsx
@@ -1,6 +1,6 @@
 import { create } from 'apisauce';
 export const api = create({
-  baseURL: 'https://accuratebackend.herokuapp.com/',
+  baseURL: 'https://accuratebackend.herokuapp.com/api/v1/',
   headers: {
     Accept: 'application/vnd.github.v3+json',
     'Content-Type': 'application/json',
