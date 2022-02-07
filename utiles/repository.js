a02394091fe3bcbd9e077dc751bba6abbf8f91c7
aiahsan <ai.ahsanismail@gmail.com>
2022-02-07 13:10:15 +0500

    web update

--- utiles/repository.js
+++ utiles/repository.js
@@ -1,11 +1,18 @@
-import { api } from './baseUrl';
+const sendMail = async (body) => {
+  const request = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/contact`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  const { status } = request;
+  if (status === 204) {
+    return { status };
+  }
+  const data = await request.json();
 
- 
-const contact = async (data) => {
-   return await api.post('/dashboard', data);
+  return { data, status };
 };
 
-export const repository = {
- 
-  contact,
-};
+export const repository = { sendMail };
